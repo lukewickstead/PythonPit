7c3c9fc66a867fb52ef66371089c73261206b3e0
luke <lwickstead@googlemail.com>
2015-07-04 16:10:19 +0100

    Adding in django-debug-toolbar

--- DjangoSandBox/DjangoSandBox/DjangoSandBox/config/base.py
+++ DjangoSandBox/DjangoSandBox/DjangoSandBox/config/base.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # http://django-debug-toolbar.readthedocs.org/
+    'debug_toolbar',
+
     'helloworld',
     'modelsintroduction',
     'templatesintroduction',
