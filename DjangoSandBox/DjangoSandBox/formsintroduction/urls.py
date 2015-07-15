d0d34326ba30f3e6dabcb74c8c401a6a7b09f702
luke <lwickstead@googlemail.com>
2015-07-15 18:27:51 +0100

    Standardising urls to be Model/Id/Action

--- DjangoSandBox/DjangoSandBox/formsintroduction/urls.py
+++ DjangoSandBox/DjangoSandBox/formsintroduction/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 urlpatterns = \
     patterns('',
-             url(r'^$', views.index, name="form"),
+             url(r'^$', views.index, name="index"),
              url(r'^basic/$', views.basic, name="form_basic"),
              url(r'^basichtml/$', views.basic_html, name="form_basic_html"),
              url(r'^class/create/$', views.class_form, name="form_class_create"),
-             url(r'^class/edit(?P<pk>[0-9]+)/$', views.class_form, name="form_class_edit"),
+             url(r'^class/(?P<pk>[0-9]+)/edit/$', views.class_form, name="form_class_edit"),
              url(r'^factory/create/$', views.model_form_factory_form, name="form_factory_create"),
-             url(r'^factory/edit(?P<pk>[0-9]+)/$', views.model_form_factory_form, name="form_factory_edit"),
+             url(r'^factory/(?P<pk>[0-9]+)/edit/$', views.model_form_factory_form, name="form_factory_edit"),
              url(r'^complete_model/create/$', views.complete_model_example, name="form_complete_create"),
-             url(r'^complete_model/edit(?P<pk>[0-9]+)/$', views.complete_model_example, name="form_complete_edit")
+             url(r'^complete_model/(?P<pk>[0-9]+)/edit/$', views.complete_model_example, name="form_complete_edit")
              )
