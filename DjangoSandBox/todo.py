6d5e41f48f7f16f6a474fced2b813078098ae328
luke <lwickstead@googlemail.com>
2015-07-15 18:29:35 +0100

    Updating todo list

--- DjangoSandBox/todo.py
+++ DjangoSandBox/todo.py
@@ -1,5 +1,4 @@
-# TODO: Templates, Layout, AJAX, Admin, Web Services
-# Separate; Sessions/Cookies, Errors/Logging,
-# Helloworld; reverse/redirect
-# Test; Auth
-# Notes: Helloworld, Models, Templates, Views, Forms, Auth, Test, Sessions/Cookies, Errors/Logging
+# TODO:
+
+# Notes:    Test
+# Code:     Templates, Layout, AJAX, Admin, Web Services, Permissions, Test Auth/Sessions/Cookies, Reverse/Redirect ,JoinQuerySet
