26f04da5aa040c6237b5aa09604e13442d29c10c
lukey <lwickstead@googlemail.com>
2015-07-15 18:26:22 +0100

    Changes to model cheat sheet

--- DjangoSandBox/DjangoSandBox/modelsadvanced/models_cheat_sheet.py
+++ DjangoSandBox/DjangoSandBox/modelsadvanced/models_cheat_sheet.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator, MinLengthValidator, \
@@ -5,36 +7,39 @@ from django.core.validators import MinValueValidator, MaxValueValidator, RegexVa
 from django.utils import timezone
 
 
+# Reference
+# https://docs.djangoproject.com/en/1.8/ref/models/
+# https://docs.djangoproject.com/en/1.8/ref/models/fields
+# https://docs.djangoproject.com/en/1.8/ref/models/options/
+
+
 class StringFieldTypesExampleModel(models.Model):
-    # CharField and TextField. TextField is for larger quantities of text
-    string_field = models.CharField(max_length=10, unique=True)
-    text_field = models.TextField()
-    email_field = models.EmailField()
+    # max_length =  maximum number of characters allowed
+
+    string_field = models.CharField(max_length=10)  # Displays as a text box
+    text_field = models.TextField(max_length=100)  # Displays as a text input area instead
+    email_field = models.EmailField(max_length=100)  # Provides regex validation for a email address
+    csv_ints_field = models.CommaSeparatedIntegerField(max_length=100)  # List of csv integers
 
 
 class SpecialStringFieldTypesExampleModel(models.Model):
-    # CharField which allows restriction to certain paths!
-    field_file_field = models.FilePathField(path="/home/", match="*.txt", recursive=True)
-    ip_field = models.GenericIPAddressField(protocol="IPv4")
     slug_field = models.SlugField(max_length=50)  # [a-ZA-Z0-9-_]{0,max_length}
-    url_field = models.URLField()
+    url_field = models.URLField()  # Provides regex validation for URL
 
 
 class BooleanFieldTypesExampleModel(models.Model):
-    # BooleanFeld and NullBooleanField
-    boolean_field = models.BooleanField()
-    null_boolean_field = models.NullBooleanField()
+    boolean_field = models.BooleanField()  # true/false
+    null_boolean_field = models.NullBooleanField()  # null/true/false
 
 
 class IntegerFieldTypesExampleModel(models.Model):
     # Integers
-    id_field = models.AutoField(primary_key=True)  # Automatically added if not specified
-    bit_int_field = models.BigIntegerField()
-    int_field = models.IntegerField()
-    pos_int_field = models.PositiveIntegerField()
-    small_int_field = models.SmallIntegerField()
-    pos_small_int_field = models.PositiveSmallIntegerField
-    csv_ints_field = models.CommaSeparatedIntegerField()
+    id_field = models.AutoField(primary_key=True)  # Automatically added if not specified. IntegerField
+    bit_int_field = models.BigIntegerField()  # 64 bit; -9223372036854775808 to 9223372036854775807
+    int_field = models.IntegerField()  # 32 bit: -2147483648 to 2147483647
+    pos_int_field = models.PositiveIntegerField()  # 0 to 2147483647
+    small_int_field = models.SmallIntegerField()  # -32768 to 32767
+    pos_small_int_field = models.PositiveSmallIntegerField  # 0 to 3276
 
 
 class BinaryFieldTypesExampleModel(models.Model):
@@ -44,15 +49,33 @@ class BinaryFieldTypesExampleModel(models.Model):
 
 class NumberFieldTypesExampleModel(models.Model):
     # Floating Point Fields
-    decimal_field = models.DecimalField()
-    float_field = models.FloatField()
+
+    # max_digits = total number of digits
+    # decimal_places = total number of digits after the decimal point
+
+    decimal_field = models.DecimalField(max_digits=8, decimal_places=2)  # Python Decimal type.
+    float_field = models.FloatField()  # Python Float type.
 
 
 class DateFieldTypesExampleModel(models.Model):
     # Dates
+
+    # auto_now: Updates to now upon every save
+    # auto_now_add: Updates to now upon record creation
+
     date_field = models.DateField()
     time_field = models.TimeField()
     date_time_field = models.DateTimeField()
+
+    duration = models.DurationField()  # modeled by Python time delta
+
+
+class SpecialFieldTypesExampleModel(models.Model):
+    models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # A UUID
+
+    ip_field = models.GenericIPAddressField(protocol="IPv4")  # An IP address
+
+    field_file_field = models.FilePathField(path="/home/", match="*.txt", recursive=True)  # A file path
 
 
 class FilesFieldTypesExampleModel(models.Model):
